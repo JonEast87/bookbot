a8503f71a3d7c19e9f0214525c773c5305356bfe
jdeastin <j.d.east@icloud.com>
2024-07-27 19:22:18 -0700

    added bubbleSort to replace .sort() method

--- main.py
+++ main.py
@@ -1,40 +1,58 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read() 
-        words = file_contents.split() # Book is split into an array of it's words
+        # Book is split into an array of it's words
+        words = file_contents.split() 
         word_report = count_words(words)
         letter_report = count_letters(words)
         print_report(word_report, create_list(letter_report))
 
-# A function that takes a dictionary and returns the value of the "num" key
-def sort_on(dict):
-    return dict["num"]
+def bubbleSort(letters):
+    # Capturing length needed for the loops
+    sorted_letters = len(letters)
+    # The outer loop will increment after each pass through
+    # n-1 is to ensure loop stops at the second to last index for the final check
+    for i in range(sorted_letters-1):
+        for j in range(sorted_letters-i-1):
+            # Standard conditional check for greater than
+            if letters[j]["num"] > letters[j + 1]["num"]:
+                # Swapping of larger for smaller
+                letters[j]["num"], letters[j + 1]["num"] = letters[j+1]["num"], letters[j]["num"]
+
+    return letters
 
 def count_words(words):
     return len(words)
 
 def count_letters(words):
-    count_letters = dict() # storing all characters/counts in dictionary
+    # storing all characters/counts in dictionary
+    count_letters = dict() 
     for word in words:
-        lowercase_word = word.lower() # lowercase to ensure all matching characters are counted
-        letters = list(lowercase_word) # list will split each word into an array for easy iteration
+        # lowercase to ensure all matching characters are counted
+        lowercase_word = word.lower() 
+        # list will split each word into an array for easy iteration
+        letters = list(lowercase_word) 
         for letter in letters:
-            if letter in count_letters and letter.isalpha(): # if the character exists at index
-                count_letters[letter] += 1 # then increment
+            # if the character exists at index
+            if letter in count_letters and letter.isalpha(): 
+                # then increment
+                count_letters[letter] += 1 
             elif letter.isalpha(): 
-                count_letters[letter] = 1 # else we will create a new one for the first instance
+                # else we will create a new one for the first instance
+                count_letters[letter] = 1 
     return count_letters
 
 # Repalcing dictionary/values to list of dictionary/values for an easy sort
 def create_list(letters):
     count_letters_list = []
     for letter in letters:
-        new_dict = {"letter": letter, "num": letters[letter]}
+        # dictionaries will be added. they contain the l and value
+        new_dict = {"letter": letter, "num": letters[letter]} 
         count_letters_list.append(new_dict)
 
-    # This method knows how to sort after declaring func sort_on
-    count_letters_list.sort(reverse=True, key=sort_on)
-    return count_letters_list
+    # Implemented a bubbleSort to replace .sort()
+    sorted_letters = bubbleSort(count_letters_list)
+    return sorted_letters
 
 def print_report(words, letters):
     print(f"--- Begin report of books/frankenstein.txt ---")
